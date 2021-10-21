1cedfc8f34c33ff72b354210f93db01bbaf9672a
Manoj Kumar <manojkumarsajja@outlook.com>
2021-10-21 11:58:57 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,7 @@
+# x is an variable. 
+x = 5
+print(x)
+
+# type() method returns class type of the argument(object) passed as parameter.
+# type() function is mostly used for debugging purposes.
+print(type(x))
