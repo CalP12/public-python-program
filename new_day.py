ddabbe49f9fed814a2dd151c1c5ec1210d841937
Manoj Kumar <manojkumarsajja@outlook.com>
2021-10-21 09:40:04 +0530

    Update new_day.py

--- new_day.py
+++ new_day.py
@@ -1,1 +1,6 @@
-
+# Use print() function to print message on to screen.
+# The print() function prints the specified message to the screen, 
+# Or other standard output device.
+# The message can be a string, or any other object, 
+# The object will be converted into a string before written to the screen.
+print("Hello world")
